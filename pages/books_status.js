300d79e2653ac1c562642d0ac80a1703c08af5ed
Bryant-surface <chaoers@bu.edu>
2024-03-18 16:15:05 -0400

    class activities

--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,8 @@
 let BookInstance = require('../models/bookinstance');
 
+BookInstance.find('status': {$eq: "Available"}).populate('book').exec().then(
+    list_BookInstance
+)
 exports.show_all_books_status = function(res) {
   return res.send([]);
 }
