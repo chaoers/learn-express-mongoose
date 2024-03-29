0cf4f02042d2fa70cb00a3fbc9cded8daa94c956
Bryant-surface <chaoers@bu.edu>
2024-03-29 17:40:07 -0400

    fix sql injection eror in book_details

--- pages/book_details.js
+++ pages/book_details.js
@@ -1,23 +1,33 @@
 let async = require('async');
 let Book = require('../models/book');
 let BookInstance = require('../models/bookinstance');
+const { ObjectId } = require('mongoose').Types;
+
+function isValidObjectId(id) {
+    return ObjectId.isValid(id) && new ObjectId(id).toString() === id;
+}
+
 
 function get_book(id) {
-    if (typeof id !== "string") {
-        return ({status: "error"});
+    if (!isValidObjectId(id)) {
+        return ({ status: "error", message: "Invalid ID format" });
     }
     return Book.findOne({'_id': {$eq: id}}).populate('author');
 }
 
 function get_book_dtl(id) {
+    if (!isValidObjectId(id)) {
+        return ({ status: "error", message: "Invalid ID format" });
+    }
   return BookInstance
           .find({ 'book': id })
           .select('imprint status');
 }
 
 exports.show_book_dtls = async (res, id) => {
-  const results = await Promise.all([get_book(id).exec(), get_book_dtl(id).exec()])
+
   try {
+      const results = await Promise.all([get_book(id).exec(), get_book_dtl(id).exec()])
     let book = await results[0];
     let copies = await results[1];
     res.send({
